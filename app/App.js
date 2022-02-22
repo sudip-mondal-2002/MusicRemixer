2737656954f5558947de722788c8a82dfe633c91
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-02-22 23:32:28 +0530

    done except audio fetching

--- app/App.js
+++ app/App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [musics, setMusics] = React.useState({})
+  const [selected, setSelected] = React.useState([])
+  const add = (name) => {
+    setSelected([...selected, name])
+  }
+  const remove = (name) => {
+    var filteredArr = selected.filter(select => select != name);
+    setSelected(filteredArr)
+  }
+  const mix = async () => {
+    var urls = [];
+    urls = selected.map(filename => {
+      return musics[filename]
+    })
+    const data = { "urls": urls }
+    //TODO
+  }
+  React.useEffect(async () => {
+    const res = await fetch("https://musicremixer.herokuapp.com/music")
+    const listOfMusic = await res.json();
+    setMusics(listOfMusic["musics"])
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {
+        Object.keys(musics).map((name) => {
+          return (
+            <View key={name} style={{ height: 50, marginTop: 5, width: 300 }}>
+              {selected.filter(select => select == name).length == 0 ?
+                <Button title={`Add ${name}`} color="#2E8B57" onPress={() => { add(name) }} /> :
+                <Button title={`Delete ${name}`} color="#8B572E" onPress={() => { remove(name) }} />}
+            </View>
+          )
+        })
+      }
+      <View style={{ height: 50, marginTop: 10, width: 200 }}>
+        <Button title="Mix" onPress={mix} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +53,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  listItem: {
+    minWidth: 200
+  }
 });
