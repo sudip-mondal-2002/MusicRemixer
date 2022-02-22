6970f6c06b8202b259d8d9e9fa618304e5912f4d
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-02-23 00:48:58 +0530

    finished project

--- app/App.js
+++ app/App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import RNFetchBlob from 'rn-fetch-blob'
+import Sound from 'react-native-sound'
+Sound.setCategory('Playback');
 
 export default function App() {
   const [musics, setMusics] = React.useState({})
@@ -17,8 +20,32 @@ export default function App() {
     urls = selected.map(filename => {
       return musics[filename]
     })
-    const data = { "urls": urls }
-    //TODO
+    try {
+      const blob = await RNFetchBlob.config({
+        fileCache: true
+      }).fetch('POST', "https://musicremixer.herokuapp.com/remix", {
+        "Content-Type": "application/json",
+      }, JSON.stringify({ urls }))
+      var music = new Sound(blob.path(), Sound.MAIN_BUNDLE, (error) => {
+        if (error) {
+          console.log('failed to load the sound', error);
+          return;
+        }
+        // loaded successfully
+        console.log('duration in seconds: ' + music.getDuration() + 'number of channels: ' + music.getNumberOfChannels());
+
+        // Play the sound with an onEnd callback
+        music.play((success) => {
+          if (success) {
+            console.log('successfully finished playing');
+          } else {
+            console.log('playback failed due to audio decoding errors');
+          }
+        })
+      })
+    } catch (error) {
+
+    }
   }
   React.useEffect(async () => {
     const res = await fetch("https://musicremixer.herokuapp.com/music")
@@ -32,7 +59,7 @@ export default function App() {
           return (
             <View key={name} style={{ height: 50, marginTop: 5, width: 300 }}>
               {selected.filter(select => select == name).length == 0 ?
-                <Button title={`Add ${name}`} color="#2E8B57" onPress={() => { add(name) }} /> :
+                <Button title={`Add ${name}`} color="#2E8B57" onPress={() => { add(name) }} disabled={selected.length>=3} /> :
                 <Button title={`Delete ${name}`} color="#8B572E" onPress={() => { remove(name) }} />}
             </View>
           )
